6c8ac12f3738a5cf650c2203a4e99a7c94b8b837
xiefengdaxia <1005928472@qq.com>
2015-12-15 22:19:26 +0800

    C#多线程代理IP采集和验证

---
+++ 代理ip抓取/代理ip抓取/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace 代理ip抓取
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(521, 397);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "代理验证";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "抓西刺代理";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "验证:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 101);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "导入txt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "计数:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(529, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(358, 202);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "设置";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(286, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "线程数:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(3, 53);
+            this.trackBar1.Maximum = 200;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(286, 45);
+            this.trackBar1.SmallChange = 10;
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Value = 50;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(103, 20);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "抓004388";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 400);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "代理ip抓取验证小工具 By1005928472@qq.com";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ 代理ip抓取/代理ip抓取/Form1.cs
@@ -0,0 +1,421 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace 代理ip抓取
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+        }
+        private int maxThread = 50;//默认可执行线程最大数量
+        private static System.Threading.Mutex muxConsole = new System.Threading.Mutex();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            showThreadCount();
+        }
+        private void showThreadCount()
+        {
+            label3.Text = "线程数:" + trackBar1.Value;
+            maxThread = trackBar1.Value;
+        }
+        private void GetProxyIp(object url)
+        {
+
+            try
+            {
+                //string Cookie = "_free_proxy_session=BAh7B0kiD3Nlc3Npb25faWQGOgZFVEkiJThiZjU2NDZlZjljOWIxY2ExODA4NTEwYTM1ZjVlMTlmBjsAVEkiEF9jc3JmX3Rva2VuBjsARkkiMThxV1Nza2o0QVNBZG1YdHRVYzRibm5OQkQwckt3V3FwL04wL3RNUDJMU1E9BjsARg%3D%3D--784d7667471701a7b9941447ccef4f4f36935fae; CNZZDATA4793016=cnzz_eid%3D1324463153-1448595402-%26ntime%3D1448595402";
+                string userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari/537.36 SE 2.X MetaSr 1.0";
+                string html = helper.GetResponseString(helper.CreateGetHttpResponse(url.ToString(), 50, userAgent, null));
+                saveIpTxt(过滤438(html), "西刺代理");
+            }
+            catch
+            {
+                //throw ex;
+            }
+            //return html;
+        }
+        //public static string ClearHtmlExceptA(string html)
+        //{
+        //    string acceptable = "tr";
+        //    string stringPattern = @"</?(?(?=" + acceptable + @")notag|[a-zA-Z0-9]+)(?:\s[a-zA-Z0-9\-]+=?(?:(["",']?).*?\1?)?)*\s*/?>";
+        //    html = Regex.Replace(html, stringPattern, "");
+        //    html = Regex.Replace(html, @"[\t\n]", "", RegexOptions.IgnoreCase);
+        //    html = Regex.Replace(html, @"[\r]", "", RegexOptions.IgnoreCase);
+        //    //html = Regex.Replace(html, @"[\t\n\r\s]","",RegexOptions.IgnoreCase);
+        //    return html;
+        //}
+        public static string 过滤(string html)
+        {
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            StringBuilder sb = new StringBuilder();
+
+            HtmlNode node = doc.DocumentNode.SelectSingleNode("//*[@id='ip_list']");////*[@id="ip_list"]/tbody
+            IEnumerable<HtmlNode> nodeList = node.ChildNodes;  //获取该元素所有的父节点的集合
+            foreach (HtmlNode item in nodeList)
+            {
+                if (item.InnerText.Length > 10 && item.InnerText.Contains("."))
+                {
+                    string a = Regex.Replace(item.InnerText, @"[\n\r]", "-", RegexOptions.IgnoreCase).Replace(" ", "");
+                    //采集格式"---115.46.100.118-8123--广西南宁--高匿-HTTP---------------15-11-2712:00-";
+                    string[] arr = a.Split(new string[] { "-" }, StringSplitOptions.RemoveEmptyEntries);
+                    sb.AppendLine(arr[0] + ":" + arr[1]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static string 过滤438(string html)
+        {
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            StringBuilder sb = new StringBuilder();
+
+            HtmlNode node = doc.DocumentNode.SelectSingleNode("/html/body/table[3]/tbody[1]/tr/td/table[2]");///html/body/table[3]/tbody/tr/td/table[2]/tbody/tr/td/table[2]/tbody/tr/td
+            IEnumerable<HtmlNode> nodeList = node.ChildNodes;  //获取该元素所有的父节点的集合
+            foreach (HtmlNode item in nodeList)
+            {
+                MessageBox.Show(item.InnerText);
+                if (item.InnerText.Length > 10 && item.InnerText.Contains("."))
+                {
+                    string a = Regex.Replace(item.InnerText, @"[\n\r]", "-", RegexOptions.IgnoreCase).Replace(" ", "");
+                    //采集格式"---115.46.100.118-8123--广西南宁--高匿-HTTP---------------15-11-2712:00-";
+                    string[] arr = a.Split(new string[] { "-" }, StringSplitOptions.RemoveEmptyEntries);
+                    sb.AppendLine(arr[0] + ":" + arr[1]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static object locker = new object();
+        public static void saveIpTxt(string ips, string txtName)
+        {
+            lock (locker)
+            {
+                if (File.Exists(Application.StartupPath + "\\" + txtName + ".txt"))
+                {
+                    StreamWriter streamWriter = File.AppendText(txtName + ".txt");
+                    streamWriter.WriteLine(ips);
+                    streamWriter.Close();
+                }
+                else
+                {
+                    StreamWriter streamWriter = File.CreateText(txtName + ".txt");
+                    streamWriter.WriteLine(ips);
+                    streamWriter.Close();
+                }
+            }
+        }
+        public ArrayList ipArr;
+        public long linecount = 0;
+        public static queueresult[] qr;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (path == "")
+            {
+                MessageBox.Show("你还没有导入txt！", "友情提示");
+                return;
+            }
+            qr = new queueresult[maxThread];
+            checkedCount = 0;
+            gaoni = 0;
+            touming = 0;
+            fail = 0;
+            queueID = 0;
+            Thread worker = new Thread(delegate()
+            {
+                try
+                {
+                    for (int i = 0; i < maxThread; i++)
+                    {
+                        // 创建指定数量的线程
+                        // 是线程调用Run方法
+                        // 启动线程
+                        Thread trd = new Thread(new ParameterizedThreadStart(checkProxyIp));
+                        trd.Name = i.ToString();
+                        trd.Start(i);
+                        ShowText(trd.Name + "已经创建！\n\r");
+                        trd.IsBackground = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            });
+            worker.Start();
+        }
+
+        public int checkedCount = 0;
+        public int gaoni = 0;
+        public int touming = 0;
+        public int fail = 0;
+        public int queueID = 0;
+
+
+        public void ShowText(string str)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke((Action<String>)ShowText, str);
+                return;
+            }
+            this.richTextBox1.AppendText(str);
+        }
+
+        public void UpdateLabel(queueresult r)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke((Action<queueresult>)UpdateLabel, r);
+                return;
+            }
+            checkedCount += r.Checkedcount;
+            fail += r.Fail;
+            gaoni += r.Gaoni;
+            touming += r.Touming;
+            label1.Text = "验证：" + checkedCount + "||失败:" + fail + "||透明:" + touming + "||高匿:" + gaoni;
+        }
+        private void checkProxyIp(object i)
+        {
+
+            try
+            {
+                //bool releasedFlag = false;
+                //muxConsole.WaitOne(); //阻塞队列  
+                //Interlocked.Increment(ref poolFlag);//标记+1  
+                //if (poolFlag != maxThread) //判断是否等于上限  
+                //{
+                //    muxConsole.ReleaseMutex(); //如果此线程达不到可执行线程上限,则继续开通,让后面的线程进来  
+                //    releasedFlag = true;
+                //}
+
+                var queueID = (int)i;
+                queueresult q = qr[queueID] = new queueresult();
+                ShowText(i + "线程开始,队列id：" + queueID + "需要处理数" + qrr[queueID].Count + "\n\r");
+                //while (qrr[queueID].Count != 0)
+                while (qrr[queueID].Count > 0)
+                {
+                    var r = helper.DownLoadHtml("http://ip.cn/", qrr[queueID].Dequeue().ToString(), q);
+                    //checkedCount = 0;
+                    //fail = 0;
+                    //gaoni = 0;
+                    //touming = 0;
+                    //lock (helper.count_locker)
+                    //{
+                    UpdateLabel(r);
+                }
+
+                ShowText("队列" + i + "验证：" + q.Checkedcount + "||失败:" + q.Fail + "||透明:" + q.Touming + "||高匿:" + q.Gaoni + "\n\r");
+                //label1.Text = "验证：" + q.Checkedcount + "||失败:" + q.Fail + "||透明:" + q.Touming + "||高匿:" + q.Gaoni;
+                //Interlocked.Decrement(ref poolFlag);
+                //if (!releasedFlag) muxConsole.ReleaseMutex();
+                //ShowText(Thread.CurrentThread.Name + "线程已经停止运行...\n\r");
+            }
+
+            catch (Exception ex)
+            {
+                ShowText("队列id：" + i + "\n\r" + ex.Message + "\n\r" + ex.StackTrace + "\n\r");
+            }
+
+        }
+        /// <summary>
+        /// 队列结果
+        /// </summary>
+        public class queueresult
+        {
+            private int checkedcount = 0;
+
+            public int Checkedcount
+            {
+                get { return checkedcount; }
+                set { checkedcount = value; }
+            }
+            private int fail = 0;
+
+            public int Fail
+            {
+                get { return fail; }
+                set { fail = value; }
+            }
+            private int touming = 0;
+
+            public int Touming
+            {
+                get { return touming; }
+                set { touming = value; }
+            }
+            private int gaoni = 0;
+
+            public int Gaoni
+            {
+                get { return gaoni; }
+                set { gaoni = value; }
+            }
+        }
+        /// <summary>
+        /// 抓取西刺代理ip
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 1; i <= 388; i++)
+                {
+
+                    ThreadPool.QueueUserWorkItem(this.GetProxyIp, "http://www.xicidaili.com/nn/" + i);
+
+                }
+
+                for (int i = 1; i <= 162; i++)
+                {
+                    ThreadPool.QueueUserWorkItem(this.GetProxyIp, "http://www.xicidaili.com/wn/" + i);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.StackTrace);
+            }
+        }
+
+        Queue[] qrr;
+        string path = string.Empty;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Title = "打开";
+                ofd.Filter = "所有文件|*.txt";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    path = ofd.FileName;
+                }
+                if (path == "")
+                {
+                    return;
+                }
+                qrr = new Queue[maxThread];
+                Thread worker = new Thread(delegate()
+                {
+                    DateTime dt_read_ip_txt = DateTime.Now;
+                    string content = string.Empty;
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        content = sr.ReadToEnd();//一次性读入内存
+                    }
+                    MemoryStream ms = new MemoryStream(Encoding.GetEncoding("GB2312").GetBytes(content));//放入内存流，以便逐行读取
+                    linecount = 0;
+                    string line = string.Empty;
+                    ipArr = new ArrayList();
+                    Queue myQ = new Queue();
+
+
+                    using (StreamReader sr = new StreamReader(ms))
+                    {
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if (line.Trim() != "")
+                            {
+                                myQ.Enqueue(line);
+                                linecount++;
+                            }
+                        }
+                        label2.Text = "导入计数:" + myQ.Count;
+
+                    }
+                    TimeSpan ts = DateTime.Now - dt_read_ip_txt;
+                    label2.Text += "条，读取用时:" + ts.TotalMilliseconds + "ms";
+
+                    int a = 0;
+                    double oneQueCount = Math.Ceiling((myQ.Count) / (Convert.ToDouble(maxThread)));
+                    for (int i = 0; i < maxThread; i++)
+                    {
+                        qrr[i] = new Queue();
+                        for (int j = 0; j < oneQueCount; j++)
+                        {
+                            if (myQ.Count != 0)
+                            {
+                                qrr[i].Enqueue(myQ.Dequeue().ToString());
+
+                            }
+                        }
+                        a += qrr[i].Count;
+                    }
+
+                    MessageBox.Show("总数:" + a + "条，已分" + qrr.Length + "小队逐个击破！", "友情提示");
+                });
+                worker.IsBackground = true;
+                worker.Start();
+                //worker.Join();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace, ex.Message);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_ValueChanged_1(object sender, EventArgs e)
+        {
+            showThreadCount();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //html/body/table[3]/tbody/tr/td/table[2]/tbody/tr/td/table[2]/tbody/tr/td
+            //
+            try
+            {
+                //for (int i = 1; i <= 388; i++)
+                //{
+
+                    ThreadPool.QueueUserWorkItem(this.GetProxyIp, "http://www.004388.com/ip/index.html");
+
+                //}
+
+                //for (int i = 1; i <= 162; i++)
+                //{
+                //    ThreadPool.QueueUserWorkItem(this.GetProxyIp, "http://www.xicidaili.com/wn/" + i);
+                //}
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.StackTrace);
+            }
+        }
+    }
+}
+
+
