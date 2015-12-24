76f10af37e2b398ac4d69c8a830b6d188c816048
xiefeng <1005928472@qq.com>
2015-12-24 17:09:17 +0800

    开始增加wpf版本

---
+++ 代理ip抓取/代理ip抓取forWPF/MainWindow.xaml.cs
@@ -0,0 +1,404 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 代理ip抓取forWPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Dispatcher.Invoke(new Action(() => { }));
+            this.Loaded += Window_Loaded;
+        }
+        //数据库连接
+        SQLiteConnection m_dbConnection;
+        SQLiteDataAdapter da;
+        DataSet ds;
+        //创建一个连接到指定数据库
+        void connectToDatabase()
+        {
+            m_dbConnection = new SQLiteConnection("Data Source=123.sqlite;Version=3;");
+            m_dbConnection.Open();
+        }
+        private void showDataGV()
+        {
+            SQLiteCommand cmd = new SQLiteCommand(m_dbConnection);//实例化SQL命令
+            cmd.CommandText = "select * from proxyIpLinksSource";
+            //DataSet ds=SQLiteHelper.ExecuteDataset(cmd);
+            ds = new DataSet();
+            da = new SQLiteDataAdapter(cmd);
+            da.Fill(ds);
+            //da.Dispose();
+            cmd.Connection.Close();
+            //cmd.Dispose();
+            DG.ItemsSource = ds.Tables[0].DefaultView;
+        }
+        private int maxThread = 100;//默认可执行线程最大数量
+        private void showThreadCount()
+        {
+            threadcount.Content = "线程数:" + slider_01.Value;
+            maxThread = Convert.ToInt32(slider_01.Value);
+        }
+        private void Window_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// 在此处添加事件处理程序实现。
+            //showThreadCount();
+            connectToDatabase();
+            showDataGV();
+            DG.LoadingRow += new EventHandler<DataGridRowEventArgs>(DG_LoadingRow); 
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// 在此处添加事件处理程序实现。
+            SQLiteCommandBuilder bu = new SQLiteCommandBuilder(da);
+            da.Update(ds, "Table");
+            MessageBox.Show("保存成功！");
+            showDataGV();
+        }
+
+        private void DG_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = e.Row.GetIndex() + 1;    //设置行表头的内容值   
+        }
+
+        public static string 过滤(string html)
+        {
+            StringBuilder sb = new StringBuilder();
+            var regex = @"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:\d{1,5}\b";
+            Regex r = new Regex(regex, RegexOptions.IgnoreCase);
+            var rawRes = html;
+            Match m = r.Match(html);
+            while (m.Success)
+            {
+                sb.AppendLine(m.Value);
+                m = m.NextMatch();
+            }
+
+            return sb.ToString();
+        }
+
+        public static string 过滤(string html, string xpath)
+        {
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            StringBuilder sb = new StringBuilder();
+
+            HtmlNode node = doc.DocumentNode.SelectSingleNode(xpath);
+            IEnumerable<HtmlNode> nodeList = node.ChildNodes;  //获取该元素所有的父节点的集合
+            foreach (HtmlNode item in nodeList)
+            {
+                //MessageBox.Show(item.InnerText);
+                if (item.InnerText.Length > 10 && item.InnerText.Contains("."))
+                {
+                    string a = Regex.Replace(item.InnerText, @"[\n\r]", "-", RegexOptions.IgnoreCase).Replace(" ", "");
+                    //采集格式"---115.46.100.118-8123--广西南宁--高匿-HTTP---------------15-11-2712:00-";
+                    string[] arr = a.Split(new string[] { "-" }, StringSplitOptions.RemoveEmptyEntries);
+                    sb.AppendLine(arr[0] + ":" + arr[1]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static object locker = new object();
+        public static string txtpath ="\\代理ip\\";
+        public static void saveIpTxt(string ips, string txtName)
+        {
+            lock (locker)
+            {
+
+                if (File.Exists(txtpath + txtName + ".txt"))
+                {
+                    StreamWriter streamWriter = File.AppendText(txtpath + txtName + ".txt");
+                    streamWriter.WriteLine(ips);
+                    streamWriter.Close();
+                }
+                else
+                {
+                    StreamWriter streamWriter = File.CreateText(txtpath + txtName + ".txt");
+                    streamWriter.WriteLine(ips);
+                    streamWriter.Close();
+                }
+            }
+        }
+        public ArrayList ipArr;
+        public long linecount = 0;
+        public static queueresult[] qr;
+        /// <summary>
+        /// 队列结果
+        /// </summary>
+        public class queueresult
+        {
+            private int checkedcount = 0;
+
+            public int Checkedcount
+            {
+                get { return checkedcount; }
+                set { checkedcount = value; }
+            }
+            private int fail = 0;
+
+            public int Fail
+            {
+                get { return fail; }
+                set { fail = value; }
+            }
+            private int touming = 0;
+
+            public int Touming
+            {
+                get { return touming; }
+                set { touming = value; }
+            }
+            private int gaoni = 0;
+
+            public int Gaoni
+            {
+                get { return gaoni; }
+                set { gaoni = value; }
+            }
+        }
+        private void _catch_Click(object sender, RoutedEventArgs e)
+        {
+            GetHTML_SaveProxyIpMethod gs;
+            try
+            {
+                Thread createWorker = new Thread(delegate()
+                {
+                    SQLiteCommand cmd = new SQLiteCommand(m_dbConnection);//实例化SQL命令
+                    cmd.CommandText = "select * from proxyIpLinksSource where checked=1";
+                    if (cmd.Connection.State == ConnectionState.Closed)
+                        cmd.Connection.Open();
+                    SQLiteDataReader idr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    while (idr.Read())
+                    {
+                        //MessageBox.Show(idr[0].ToString());
+                        gs = new GetHTML_SaveProxyIpMethod(richTextBox1);
+                        gs.Url = idr["Url"].ToString();
+                        gs.Url_plus = idr["Url_plus"].ToString();
+                        gs.Xpath = idr["Xpath"].ToString();
+                        gs.Start = Convert.ToInt32(idr["Start"]);
+                        gs.End = Convert.ToInt32(idr["End"]);
+                        gs.TxtName = idr["TxtName"].ToString();
+                        Thread worker = new Thread(delegate()
+                        {
+                            gs.DoWork();
+                        });
+                        worker.IsBackground = true;
+                        worker.Start();
+                        worker.Join();
+                    }
+                });
+                createWorker.IsBackground = true;
+                createWorker.Start();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+   
+    /// <summary>
+    /// 根据网址下载HTML源码
+    /// 使用解析利器HtmlAgilityPack过滤HTML内容
+    /// 生成指定格式保存到txt文件
+    /// </summary>
+    public class GetHTML_SaveProxyIpMethod
+    {
+        private string url;
+
+        public string Url
+        {
+            get { return url; }
+            set { url = value; }
+        }
+        private string url_plus;
+
+        public string Url_plus
+        {
+            get { return url_plus; }
+            set { url_plus = value; }
+        }
+        private string xpath;
+
+        public string Xpath
+        {
+            get { return xpath; }
+            set { xpath = value; }
+        }
+        private string txtName;
+
+        public string TxtName
+        {
+            get { return txtName; }
+            set { txtName = value; }
+        }
+        private int start;
+
+        public int Start
+        {
+            get { return start; }
+            set { start = value; }
+        }
+        private int end;
+
+        public int End
+        {
+            get { return end; }
+            set { end = value; }
+        }
+        
+        RichTextBox rtb;
+
+        public GetHTML_SaveProxyIpMethod(RichTextBox rtb)
+        {
+            this.rtb = rtb;
+        }
+        //public void appendLine(string text)
+        //{
+        //    Action<string> actionDelegate = (x) =>
+        //    {
+
+        //        this.rtb.AppendText(text + "\r\n");
+        //    };
+        //    this.Dispatcher.Invoke(actionDelegate, text);
+        //}
+
+        public static string 过滤(string html)
+        {
+            StringBuilder sb = new StringBuilder();
+            var regex = @"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:\d{1,5}\b";
+            Regex r = new Regex(regex, RegexOptions.IgnoreCase);
+            var rawRes = html;
+            Match m = r.Match(html);
+            while (m.Success)
+            {
+                sb.AppendLine(m.Value);
+                m = m.NextMatch();
+            }
+
+            return sb.ToString();
+        }
+
+        public static string 过滤(string html, string xpath)
+        {
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            StringBuilder sb = new StringBuilder();
+
+            HtmlNode node = doc.DocumentNode.SelectSingleNode(xpath);
+            IEnumerable<HtmlNode> nodeList = node.ChildNodes;  //获取该元素所有的父节点的集合
+            foreach (HtmlNode item in nodeList)
+            {
+                //MessageBox.Show(item.InnerText);
+                if (item.InnerText.Length > 10 && item.InnerText.Contains("."))
+                {
+                    string a = Regex.Replace(item.InnerText, @"[\n\r]", "-", RegexOptions.IgnoreCase).Replace(" ", "");
+                    //采集格式"---115.46.100.118-8123--广西南宁--高匿-HTTP---------------15-11-2712:00-";
+                    string[] arr = a.Split(new string[] { "-" }, StringSplitOptions.RemoveEmptyEntries);
+                    sb.AppendLine(arr[0] + ":" + arr[1]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static object locker = new object();
+        public static string txtpath = "\\代理ip\\";
+        public static void saveIpTxt(string ips, string txtName)
+        {
+            lock (locker)
+            {
+
+                if (File.Exists(txtpath + txtName + ".txt"))
+                {
+                    StreamWriter streamWriter = File.AppendText(txtpath + txtName + ".txt");
+                    streamWriter.WriteLine(ips);
+                    streamWriter.Close();
+                }
+                else
+                {
+                    StreamWriter streamWriter = File.CreateText(txtpath + txtName + ".txt");
+                    streamWriter.WriteLine(ips);
+                    streamWriter.Close();
+                }
+            }
+        }
+        public void GetHTML_SaveProxyIp(string url, string xpath, string txtName, int model)
+        {
+
+            try
+            {
+                //string Cookie = "_free_proxy_session=BAh7B0kiD3Nlc3Npb25faWQGOgZFVEkiJThiZjU2NDZlZjljOWIxY2ExODA4NTEwYTM1ZjVlMTlmBjsAVEkiEF9jc3JmX3Rva2VuBjsARkkiMThxV1Nza2o0QVNBZG1YdHRVYzRibm5OQkQwckt3V3FwL04wL3RNUDJMU1E9BjsARg%3D%3D--784d7667471701a7b9941447ccef4f4f36935fae; CNZZDATA4793016=cnzz_eid%3D1324463153-1448595402-%26ntime%3D1448595402";
+                string userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari/537.36 SE 2.X MetaSr 1.0";
+                string html = helper.GetResponseString(helper.CreateGetHttpResponse(url, 20, userAgent, null));
+                if (model == 0)
+                {
+                    saveIpTxt(过滤(html), txtName);
+                }
+                else
+                {
+                    saveIpTxt(过滤(html, xpath), txtName);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void DoWork()
+        {
+            try
+            {
+                //appendLine(DateTime.Now + "\n" + txtName + url + "开始采集！");
+                if (start == 0)
+                {
+                    GetHTML_SaveProxyIp(url, xpath, txtName, start);
+                }
+                else
+                {
+                    for (int i = start; i <= end; i++)
+                    {
+
+                        GetHTML_SaveProxyIp(url + i + url_plus, xpath, txtName, start);
+
+                    }
+                }
+                //appendLine(DateTime.Now + "\n" + txtName + url + "采集完毕！\r");
+            }
+            catch (Exception ex)
+            {
+                //appendLine(DateTime.Now + "\n" + txtName + url + "采集出错！\n" + ex.Message);
+                MessageBox.Show(ex.StackTrace, txtName + ">>" + ex.Message);
+            }
+        }
+    }
+}
---
+++ 代理ip抓取/代理ip抓取forWPF/helper.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+
+namespace 代理ip抓取forWPF
+{
+    class helper
+    {
+
+            public static 代理ip抓取forWPF.MainWindow.queueresult DownLoadHtml(string url, string proxyip, 代理ip抓取forWPF.MainWindow.queueresult q, string localhostIpAddress, int timeout = 10, bool enableProxy = true)
+            {
+                var result = new 代理ip抓取forWPF.MainWindow.queueresult();
+                try
+                {
+                    lock (q)
+                    {
+                        q.Checkedcount++;
+                        result.Checkedcount++;
+                    }
+                    string html = "";
+                    string[] arr = proxyip.Split(':');
+                    var myRequest = (HttpWebRequest)WebRequest.Create(url);
+                    myRequest.Method = "GET";
+                    myRequest.Timeout = 1000 * timeout;
+                    myRequest.AllowAutoRedirect = true;
+                    myRequest.UserAgent = "HttpClient";
+                    myRequest.Accept = "text/*";
+                    if (enableProxy)
+                    {
+                        //如果启用WEBPROXY代理
+                        var webProxy = new WebProxy(arr[0], int.Parse(arr[1]));
+                        myRequest.Proxy = webProxy;
+                    }
+                    var myResponse = (HttpWebResponse)myRequest.GetResponse();
+                    using (var sr = new StreamReader(myResponse.GetResponseStream(), Encoding.GetEncoding((myResponse.CharacterSet))))
+                    {
+                        html = sr.ReadToEnd();
+                        myResponse.Close();
+                    }
+                    if (html.Length > 10)
+                    {
+                        if (!html.Contains("111.13.62.153"))
+                        {
+                            saveIpTxt(proxyip, "验证可用√√高匿代理ip");
+                            lock (q)
+                            {
+                                q.Gaoni++;
+                                result.Gaoni++;
+                            }
+                        }
+                        else
+                        {
+                            saveIpTxt(proxyip, "验证可用√透明代理ip");
+                            lock (q)
+                            {
+                                q.Touming++;
+                                result.Touming++;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        lock (q)
+                        {
+                            q.Fail++;
+                            result.Fail++;
+                        }
+                    }
+                    //return html;
+                }
+                catch (Exception)
+                {
+                    lock (q)
+                    {
+                        q.Fail++;
+                        result.Fail++;
+                    }
+                    //return null;
+                    //throw new Exception(ex.Message);
+                }
+                finally
+                {
+
+                }
+                return result;
+            }
+            public static string path = "\\代理ip\\";
+            public static object locker = new object();
+            public static void saveIpTxt(string ips, string txtName)
+            {
+                lock (locker)
+                {
+                    if (File.Exists(path + txtName + ".txt"))
+                    {
+                        StreamWriter streamWriter = File.AppendText(path + txtName + ".txt");
+                        streamWriter.WriteLine(ips);
+                        streamWriter.Close();
+                    }
+                    else
+                    {
+                        StreamWriter streamWriter = File.CreateText(path + txtName + ".txt");
+                        streamWriter.WriteLine(ips);
+                        streamWriter.Close();
+                    }
+                }
+            }
+            /// <summary>  
+            /// 创建GET方式的HTTP请求  
+            /// </summary>  
+            public static HttpWebResponse CreateGetHttpResponse(string url, int timeout, string userAgent, CookieCollection cookies)
+            {
+                HttpWebRequest request = null;
+                if (url.StartsWith("https", StringComparison.OrdinalIgnoreCase))
+                {
+                    //对服务端证书进行有效性校验（非第三方权威机构颁发的证书，如自己生成的，不进行验证，这里返回true）
+                    ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+                    request = WebRequest.Create(url) as HttpWebRequest;
+                    request.ProtocolVersion = HttpVersion.Version10;    //http版本，默认是1.1,这里设置为1.0
+                }
+                else
+                {
+                    request = WebRequest.Create(url) as HttpWebRequest;
+                }
+                request.Method = "GET";
+                //设置代理UserAgent和超时
+                request.UserAgent = userAgent;
+                request.Headers.Add("Accept-Encoding", "gzip");
+                //request.Timeout = timeout*1000;
+                if (cookies != null)
+                {
+                    request.CookieContainer = new CookieContainer();
+                    request.CookieContainer.Add(cookies);
+                }
+                return request.GetResponse() as HttpWebResponse;
+            }
+
+            /// <summary>  
+            /// 创建POST方式的HTTP请求  
+            /// </summary>  
+            public static HttpWebResponse CreatePostHttpResponse(string url, IDictionary<string, string> parameters, int timeout, string userAgent, CookieCollection cookies)
+            {
+                HttpWebRequest request = null;
+                //如果是发送HTTPS请求  
+                if (url.StartsWith("https", StringComparison.OrdinalIgnoreCase))
+                {
+                    //ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+                    request = WebRequest.Create(url) as HttpWebRequest;
+                    request.ProtocolVersion = HttpVersion.Version10;
+                }
+                else
+                {
+                    request = WebRequest.Create(url) as HttpWebRequest;
+                }
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+
+                //设置代理UserAgent和超时
+                request.UserAgent = userAgent;
+                request.Timeout = timeout;
+
+                if (cookies != null)
+                {
+                    request.CookieContainer = new CookieContainer();
+                    request.CookieContainer.Add(cookies);
+                }
+                //发送POST数据  
+                if (!(parameters == null || parameters.Count == 0))
+                {
+                    StringBuilder buffer = new StringBuilder();
+                    int i = 0;
+                    foreach (string key in parameters.Keys)
+                    {
+                        if (i > 0)
+                        {
+                            buffer.AppendFormat("&{0}={1}", key, parameters[key]);
+                        }
+                        else
+                        {
+                            buffer.AppendFormat("{0}={1}", key, parameters[key]);
+                            i++;
+                        }
+                    }
+                    byte[] data = Encoding.ASCII.GetBytes(buffer.ToString());
+                    using (Stream stream = request.GetRequestStream())
+                    {
+                        stream.Write(data, 0, data.Length);
+                    }
+                }
+                string[] values = request.Headers.GetValues("Content-Type");
+                return request.GetResponse() as HttpWebResponse;
+            }
+
+            /// <summary>
+            /// 获取请求的数据
+            /// </summary>
+            public static string GetResponseString(HttpWebResponse webresponse)
+            {
+                if (webresponse.ContentEncoding.ToLower().Contains("gzip"))
+                {
+
+
+
+                    using (GZipStream stream = new GZipStream(
+
+                        webresponse.GetResponseStream(), CompressionMode.Decompress))
+                    {
+
+                        using (StreamReader reader = new StreamReader(stream, Encoding.Default))
+                        {
+
+                            return reader.ReadToEnd();
+
+                        }
+
+                    }
+
+                }
+                else if (webresponse.ContentEncoding.ToLower().Contains("deflate"))
+                {
+
+                    using (DeflateStream stream = new DeflateStream(
+
+                        webresponse.GetResponseStream(), CompressionMode.Decompress))
+                    {
+
+                        using (StreamReader reader =
+
+                            new StreamReader(stream, Encoding.UTF8))
+                        {
+
+                            return reader.ReadToEnd();
+
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    using (Stream s = webresponse.GetResponseStream())
+                    {
+                        StreamReader reader = new StreamReader(s, Encoding.UTF8);
+                        return reader.ReadToEnd();
+
+                    }
+                }
+
+            }
+
+            /// <summary>
+            /// 验证证书
+            /// </summary>
+            private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+            {
+                if (errors == SslPolicyErrors.None)
+                    return true;
+                return false;
+            }
+        }
+    }
+
